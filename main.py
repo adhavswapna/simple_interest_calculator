9240d0f1178a8f6c878a68b675feb11530129a9c
adhavswapna <106309276+adhavswapna@users.noreply.github.com>
2023-05-19 13:56:45 +0530

    Compound Interest

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# Compound interest is the interest on savings calculated on both the initial principal and the accumulated interest from previous periods.
+
+P = "Principal Amount"
+i = "annual interest rate"
+n ="number of compounding periods"
+
+p = float(input("Enter the principal amount : "))
+
+t = float(input("Enter the number of years : "))
+
+r = float(input("Enter the rate of interest : "))
+
+# compute compound interest
+ci = p * (pow((1 + r / 100), t))
+
+print("Compound interest" , ci)
+
+
+
+
+
 
